0752caaa4a884664fe8a89e3ca4e5cfd12c55856
“github’970029315git config --global user.email “g970029315@qq.com <970029315@qq.com>
2020-08-19 14:50:26 +0800

    主页功能

---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+$(function() {
+    getUserInof()
+})
+
+function getUserInof() {
+    $.ajax({
+        url: '/my/userinfo',
+        headers: {
+            Authorization: localStorage.getItem("kolen") || ""
+        },
+        success: function(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败！')
+            }
+            // 调用 renderAvatar 渲染用户的头像
+            renderAvatar(res.data)
+        }
+    })
+}
+
+function renderAvatar(user) {
+    // 1. 获取用户的名称
+    var name = user.nickname || user.username
+        // 2. 设置欢迎的文本
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+        // 3. 按需渲染用户的头像
+    if (user.user_pic !== null) {
+        // 3.1 渲染图片头像
+        $('.layui-nav-img')
+            .attr('src', user.user_pic)
+            .show()
+        $('.user-avatar').hide()
+    } else {
+        // 3.2 渲染文本头像
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.user-avatar')
+            .html(first)
+            .show()
+    }
+}
